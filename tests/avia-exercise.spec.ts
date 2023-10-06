4d20acfc78370196ceff64c2771af729856186bd
Oscar Pacheco <oscar.pacheco@uk.sedgwick.com>
2023-10-05 19:08:22 -0600

    Exercise complete

--- tests/avia-exercise.spec.ts
+++ tests/avia-exercise.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 import HomePage from '../pages/homePage';
 import ResultsPage from '../pages/resultsPage';
+import * as data from '../data/flight_data.json';
 
 
 test.describe('Avia Sales testing excercise', () => {
@@ -18,20 +19,20 @@ test.describe('Avia Sales testing excercise', () => {
     });
 
     await test.step('Search for a no-return ticket from NYC to Berlin, date: 30 of October, 2 passengers, economy class', async () => {
-      await homePage.setParams("JFK", 'BER', '3010', '2', '', '');
+      await homePage.setParams(data.departureAirport, data.destinationAirport, data.departureDateInt, data.passengersAdults, data.returnDate, data.class);
       await homePage.submitSearch(); // also disables checkbox that opens a new tab
     });
     await test.step('Validate results page is correctly opened and entered data is retained and displayed on the results', async () => {
       await page.waitForLoadState('load'); // waits for page to be loaded
       await expect(page.url()).toContain('/search/'); // validate results page is correctly opened
-      await expect(await resultsPage.fromCityField.getAttribute('value')).toContain('John F. Kennedy International Airport');
-      await expect(await resultsPage.toCityField.getAttribute('value')).toContain('Berlin');
-      await expect(await resultsPage.departDateField.getAttribute('value')).toContain('Mon');
-      await expect(resultsPage.passengersLabel).toHaveText("2 passengers");
-      await expect(resultsPage.classLabel).toHaveText("economy");
-      await expect(resultsPage.firstResultDepCityLabel).toHaveText("New York");
-      await expect(resultsPage.firstResultDestAirportLabel).toHaveText("BER");
-      await expect(resultsPage.firstResultDepAirportLabel).toHaveText("JFK");
+      await expect(await resultsPage.fromCityField.getAttribute('value')).toContain(data.departureAirportFullName);
+      await expect(await resultsPage.toCityField.getAttribute('value')).toContain(data.destinationAirportFullName);
+      await expect(await resultsPage.departDateField.getAttribute('value')).toContain(data.fullDepartureDate);
+      await expect(resultsPage.passengersLabel).toHaveText(data.passengersAdults + data.passengersChilden + data.passengersInfants + " passengers");
+      await expect(resultsPage.classLabel).toHaveText(data.classEmpty);
+      await expect(resultsPage.firstResultDepCityLabel).toHaveText(data.departureCity);
+      await expect(resultsPage.firstResultDestAirportLabel).toHaveText(data.destinationAirport);
+      await expect(resultsPage.firstResultDepAirportLabel).toHaveText(data.departureAirport);
       await expect(resultsPage.nightThemeOn).toBeEnabled(); // validate night theme is still enabled
     });
   })
